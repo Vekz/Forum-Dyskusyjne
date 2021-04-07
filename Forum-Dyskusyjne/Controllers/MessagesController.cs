36dbee77a4e1ad6cfbbcaac8815c6cd53f067a9f
Laura Stasiulewicz <wolfgangpolska@gmail.com>
2021-04-07 21:35:24 +0200

    CRUD generated

---
+++ Forum-Dyskusyjne/Controllers/MessagesController.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Forum_Dyskusyjne.DAL;
+using Forum_Dyskusyjne.Models;
+
+namespace Forum_Dyskusyjne.Controllers
+{
+    public class MessagesController : Controller
+    {
+        private ForumDBContext db = new ForumDBContext();
+
+        // GET: Messages
+        public ActionResult Index()
+        {
+            var messages = db.Messages.Include(m => m.Receiver).Include(m => m.Sender);
+            return View(messages.ToList());
+        }
+
+        // GET: Messages/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Message message = db.Messages.Find(id);
+            if (message == null)
+            {
+                return HttpNotFound();
+            }
+            return View(message);
+        }
+
+        // GET: Messages/Create
+        public ActionResult Create()
+        {
+            ViewBag.ReceiverId = new SelectList(db.Users, "UserId", "UserName");
+            ViewBag.SenderId = new SelectList(db.Users, "UserId", "UserName");
+            return View();
+        }
+
+        // POST: Messages/Create
+        // Aby zapewnić ochronę przed atakami polegającymi na przesyłaniu dodatkowych danych, włącz określone właściwości, z którymi chcesz utworzyć powiązania.
+        // Aby uzyskać więcej szczegółów, zobacz https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "MessageId,SenderId,ReceiverId,Text,SendDate,Seen")] Message message)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Messages.Add(message);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.ReceiverId = new SelectList(db.Users, "UserId", "UserName", message.ReceiverId);
+            ViewBag.SenderId = new SelectList(db.Users, "UserId", "UserName", message.SenderId);
+            return View(message);
+        }
+
+        // GET: Messages/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Message message = db.Messages.Find(id);
+            if (message == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.ReceiverId = new SelectList(db.Users, "UserId", "UserName", message.ReceiverId);
+            ViewBag.SenderId = new SelectList(db.Users, "UserId", "UserName", message.SenderId);
+            return View(message);
+        }
+
+        // POST: Messages/Edit/5
+        // Aby zapewnić ochronę przed atakami polegającymi na przesyłaniu dodatkowych danych, włącz określone właściwości, z którymi chcesz utworzyć powiązania.
+        // Aby uzyskać więcej szczegółów, zobacz https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "MessageId,SenderId,ReceiverId,Text,SendDate,Seen")] Message message)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(message).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.ReceiverId = new SelectList(db.Users, "UserId", "UserName", message.ReceiverId);
+            ViewBag.SenderId = new SelectList(db.Users, "UserId", "UserName", message.SenderId);
+            return View(message);
+        }
+
+        // GET: Messages/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Message message = db.Messages.Find(id);
+            if (message == null)
+            {
+                return HttpNotFound();
+            }
+            return View(message);
+        }
+
+        // POST: Messages/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Message message = db.Messages.Find(id);
+            db.Messages.Remove(message);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Forum-Dyskusyjne/Controllers/PostsController.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Forum_Dyskusyjne.DAL;
+using Forum_Dyskusyjne.Models;
+
+namespace Forum_Dyskusyjne.Controllers
+{
+    public class PostsController : Controller
+    {
+        private ForumDBContext db = new ForumDBContext();
+
+        // GET: Posts
+        public ActionResult Index()
+        {
+            var posts = db.Posts.Include(p => p.Author).Include(p => p.Thread);
+            return View(posts.ToList());
+        }
+
+        // GET: Posts/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Post post = db.Posts.Find(id);
+            if (post == null)
+            {
+                return HttpNotFound();
+            }
+            return View(post);
+        }
+
+        // GET: Posts/Create
+        public ActionResult Create()
+        {
+            ViewBag.AuthorId = new SelectList(db.Users, "UserId", "UserName");
+            ViewBag.ThreadId = new SelectList(db.Threads, "ThreadId", "ThreadTitle");
+            return View();
+        }
+
+        // POST: Posts/Create
+        // Aby zapewnić ochronę przed atakami polegającymi na przesyłaniu dodatkowych danych, włącz określone właściwości, z którymi chcesz utworzyć powiązania.
+        // Aby uzyskać więcej szczegółów, zobacz https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "PostId,Body,AuthorId,CreatedTime,ThreadId")] Post post)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Posts.Add(post);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.AuthorId = new SelectList(db.Users, "UserId", "UserName", post.AuthorId);
+            ViewBag.ThreadId = new SelectList(db.Threads, "ThreadId", "ThreadTitle", post.ThreadId);
+            return View(post);
+        }
+
+        // GET: Posts/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Post post = db.Posts.Find(id);
+            if (post == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.AuthorId = new SelectList(db.Users, "UserId", "UserName", post.AuthorId);
+            ViewBag.ThreadId = new SelectList(db.Threads, "ThreadId", "ThreadTitle", post.ThreadId);
+            return View(post);
+        }
+
+        // POST: Posts/Edit/5
+        // Aby zapewnić ochronę przed atakami polegającymi na przesyłaniu dodatkowych danych, włącz określone właściwości, z którymi chcesz utworzyć powiązania.
+        // Aby uzyskać więcej szczegółów, zobacz https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "PostId,Body,AuthorId,CreatedTime,ThreadId")] Post post)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(post).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.AuthorId = new SelectList(db.Users, "UserId", "UserName", post.AuthorId);
+            ViewBag.ThreadId = new SelectList(db.Threads, "ThreadId", "ThreadTitle", post.ThreadId);
+            return View(post);
+        }
+
+        // GET: Posts/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Post post = db.Posts.Find(id);
+            if (post == null)
+            {
+                return HttpNotFound();
+            }
+            return View(post);
+        }
+
+        // POST: Posts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Post post = db.Posts.Find(id);
+            db.Posts.Remove(post);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
