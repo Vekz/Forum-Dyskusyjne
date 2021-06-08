1eff931e960b72ea3ed2aad866aaa18c0c1d2906
Vekaz <V4nII@hotmail.com>
2021-06-08 22:56:21 +0200

    Display created thread. Added function for setting avatar from Image object and displaying it correctly.

---
+++ Forum-Dyskusyjne/Controllers/ThreadController.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Mvc;
+using Forum_Dyskusyjne.DAL;
+
+namespace Forum_Dyskusyjne.Controllers
+{
+    public class ThreadController : Controller
+    {
+        private ForumDBContext db = new ForumDBContext();
+
+        public ActionResult Index(int index)
+        {
+            var forum = db.Threads.Find(index);
+            
+            return View(forum);
+        }
+    }
+}
--- Forum-Dyskusyjne/Models/User.cs
+++ Forum-Dyskusyjne/Models/User.cs
@@ -2,8 +2,11 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Drawing;
+using System.IO;
 using System.Security.Claims;
 using System.Threading.Tasks;
+using System.Web.Hosting;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 
@@ -14,6 +17,8 @@ namespace Forum_Dyskusyjne.Models
     {
         public User()
         {
+            var defaultAvatar = Image.FromFile(HostingEnvironment.MapPath("~/Static/img/user-512.png") ?? string.Empty);
+            SetAvatar(defaultAvatar); 
             CreatedTime = DateTime.UtcNow; // Set creation date to now
             Timeout = 1800.0f; // Set timeout to: 30min = 1800s
         }
@@ -40,6 +45,15 @@ namespace Forum_Dyskusyjne.Models
             var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
             // Add custom user claims here
             return userIdentity;
+        }
+
+        public void SetAvatar(Image img)
+        {
+            using (var ms = new MemoryStream())
+            {
+                img.Save(ms, img.RawFormat);
+                this.Avatar = ms.ToArray();
+            }
         }
     }
 }
